c571ab4784f2a91688ff47fa72a351e4a698ff2b
mingwugmail <mingwu@gmail.com>
2022-05-30 14:25:15 -0400

    change to use Lazy

--- rslib/src/lib.rs
+++ rslib/src/lib.rs
@@ -1,25 +1,28 @@
 
 use dashmap::DashMap;
 use crossbeam_queue::SegQueue;
-use once_cell::unsync::OnceCell;  // shall we use sync/unsync?
+use once_cell;  // ::sync::OnceCell;  // shall we use sync/unsync?
 
 // use lazy_static::lazy_static;
 
-type HDashMap = DashMap<u64, u64>;  // DashMap that stores u64 value from D side
-type HashMapsT = Vec<HDashMap>;
+type HashMapT = DashMap<u64, u64>;  // DashMap that stores u64 value from D side
+type HashMapsT = Vec<HashMapT>;
 type HandleT = usize;
+type QueueT = SegQueue<u64>;
+type QueuesT = Vec<QueueT>;
 
 // https://github.com/rust-lang-nursery/lazy-static.rs/blob/master/examples/mutex_map.rs
 /*
 lazy_static! {
   static ref HASHMAPS: HashMapsT = {
-    let mut vec = Vec::<HDashMap>::new();
+    let mut vec = Vec::<HashMapT>::new();
     vec
   };
 }
 */
 
-static mut HASHMAPS: OnceCell<HashMapsT> = OnceCell::with_value(HashMapsT::new());
+static mut HASHMAPS: once_cell::sync::Lazy<HashMapsT> = once_cell::sync::Lazy::new(HashMapsT::new);
+static mut   QUEUES: once_cell::sync::Lazy<QueuesT>   = once_cell::sync::Lazy::new(QueuesT::new);
 
 // NOTE: `dashmap_new` is NOT thread-safe, since it will modify the underlying container Vec
 // so in the most called function dashmap_get, dashmap_insert, the HASHMAPS.get() no need to be sync-ed
@@ -27,22 +30,22 @@ static mut HASHMAPS: OnceCell<HashMapsT> = OnceCell::with_value(HashMapsT::new()
 // return a handle
 #[no_mangle]
 pub unsafe extern "C" fn dashmap_new() -> HandleT {
-  let map = HDashMap::new();
-  let handle:HandleT = HASHMAPS.get().unwrap().len();
-  HASHMAPS.get_mut().unwrap().push(map);
+  let map = HashMapT::new();
+  let handle:HandleT = HASHMAPS.len();
+  HASHMAPS.push(map);
 
   return handle;
 }
 
 #[no_mangle]
 pub unsafe extern "C" fn dashmap_get(handle:HandleT, key:u64) -> u64 {
-  *(HASHMAPS.get().unwrap().get(handle).unwrap().get(&key).unwrap())
+  *(HASHMAPS.get(handle).unwrap().get(&key).unwrap())
 }
 
 // return the old val
 #[no_mangle]
 pub unsafe extern "C" fn dashmap_insert(handle:HandleT, key:u64, val:u64) -> u64 {
-  HASHMAPS.get().unwrap().get(handle).unwrap().insert(key, val).unwrap()
+  HASHMAPS.get(handle).unwrap().insert(key, val).unwrap()
 }
 
 #[cfg(test)]
