e51c679cddef6b1a92df7fc4bb23534066403eda
mingwugmail <mingwu@gmail.com>
2022-05-30 13:32:49 -0400

    change to use OnceCell

--- rslib/src/lib.rs
+++ rslib/src/lib.rs
@@ -1,37 +1,48 @@
-use std::sync::Mutex;
 
 use dashmap::DashMap;
 use crossbeam_queue::SegQueue;
+use once_cell::unsync::OnceCell;  // shall we use sync/unsync?
 
-use lazy_static::lazy_static;
+// use lazy_static::lazy_static;
 
 type HDashMap = DashMap<u64, u64>;  // DashMap that stores u64 value from D side
+type HashMapsT = Vec<HDashMap>;
 type HandleT = usize;
 
 // https://github.com/rust-lang-nursery/lazy-static.rs/blob/master/examples/mutex_map.rs
+/*
 lazy_static! {
-  static ref HASHMAPS: Mutex<Vec<HDashMap>> = {
+  static ref HASHMAPS: HashMapsT = {
     let mut vec = Vec::<HDashMap>::new();
-    Mutex::new(vec)
+    vec
   };
 }
+*/
 
+static mut HASHMAPS: OnceCell<HashMapsT> = OnceCell::with_value(HashMapsT::new());
+
+// NOTE: `dashmap_new` is NOT thread-safe, since it will modify the underlying container Vec
+// so in the most called function dashmap_get, dashmap_insert, the HASHMAPS.get() no need to be sync-ed
+// othewise, it will be very slow
 // return a handle
 #[no_mangle]
 pub unsafe extern "C" fn dashmap_new() -> HandleT {
   let map = HDashMap::new();
-  let handle:HandleT = HASHMAPS.lock().unwrap().len();
-  HASHMAPS.lock().unwrap().push(map);
+  let handle:HandleT = HASHMAPS.get().unwrap().len();
+  HASHMAPS.get_mut().unwrap().push(map);
 
   return handle;
 }
 
+#[no_mangle]
 pub unsafe extern "C" fn dashmap_get(handle:HandleT, key:u64) -> u64 {
-  *HASHMAPS.lock().unwrap().get(handle).unwrap().get(&key).unwrap()
+  *(HASHMAPS.get().unwrap().get(handle).unwrap().get(&key).unwrap())
 }
 
+// return the old val
+#[no_mangle]
 pub unsafe extern "C" fn dashmap_insert(handle:HandleT, key:u64, val:u64) -> u64 {
-  HASHMAPS.lock().unwrap().get(handle).unwrap().insert(key, val).unwrap()
+  HASHMAPS.get().unwrap().get(handle).unwrap().insert(key, val).unwrap()
 }
 
 #[cfg(test)]
