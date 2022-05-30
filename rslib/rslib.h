02be296299a9081683af50f1870f7d1c5bd051aa
mingwugmail <mingwu@gmail.com>
2022-05-30 16:43:56 -0400

    add rslib.h

---
+++ rslib/rslib.h
@@ -0,0 +1,19 @@
+#include <cstdarg>
+#include <cstdint>
+#include <cstdlib>
+#include <ostream>
+#include <new>
+
+using HandleT = uintptr_t;
+
+extern "C" {
+
+uint64_t dashmap_get(HandleT handle, uint64_t key);
+
+uint64_t dashmap_insert(HandleT handle, uint64_t key, uint64_t val);
+
+uint64_t segqueue_pop(HandleT handle);
+
+void segqueue_push(HandleT handle, uint64_t val);
+
+} // extern "C"
