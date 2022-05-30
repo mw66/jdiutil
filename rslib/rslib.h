bc456595014bba22dd3ca5a1160badc2234e5185
mingwugmail <mingwu@gmail.com>
2022-05-30 16:54:43 -0400

    made parse.expand work using rustup default nightly

--- rslib/rslib.h
+++ rslib/rslib.h
@@ -8,6 +8,10 @@ using HandleT = uintptr_t;
 
 extern "C" {
 
+HandleT dashmap_new();
+
+HandleT segqueue_new();
+
 uint64_t dashmap_get(HandleT handle, uint64_t key);
 
 uint64_t dashmap_insert(HandleT handle, uint64_t key, uint64_t val);
